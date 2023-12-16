dde1f55fea360ee81b06657e2ecc3bdbfa3788ff
Ernesto Diaz <zaidotsenre@gmail.com>
2023-12-16 15:39:20 -0500

    Added launcher icons: 	new file:   assets/images/2.0x/launcher_icon.png 	new file:   assets/images/3.0x/launcher_icon.png 	new file:   assets/images/launcher_icon.png 	new file:   assets/images/launcher_icon_l-small.png 	new file:   assets/images/launcher_icon_l.jpg
    Cleaned up main and app:
    	modified:   lib/main.dart
    	modified:   lib/src/app.dart
    
    Removed Settings:
    	deleted:    lib/src/settings/settings_controller.dart
    	deleted:    lib/src/settings/settings_service.dart
    	deleted:    lib/src/settings/settings_view.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,15 @@ import 'package:contacal/src/calorie_log/calorie_log_controller.dart';
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final settingsController = SettingsController(SettingsService());
   final calorieLogController = CalorieLogController(DateOnly(DateTime.now()));
 
-  await settingsController.loadSettings();
   await calorieLogController.loadData();
 
   runApp(MyApp(
-    settingsController: settingsController,
     calorieLogController: calorieLogController,
   ));
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -5,17 +5,9 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/services.dart';
 
-import 'settings/settings_controller.dart';
-import 'settings/settings_view.dart';
-
-/// The Widget that configures your application.
 class MyApp extends StatelessWidget {
-  const MyApp(
-      {super.key,
-      required this.settingsController,
-      required this.calorieLogController});
+  const MyApp({super.key, required this.calorieLogController});
 
-  final SettingsController settingsController;
   final CalorieLogController calorieLogController;
 
   @override
@@ -24,41 +16,25 @@ class MyApp extends StatelessWidget {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-    return ListenableBuilder(
-      listenable: settingsController,
-      builder: (BuildContext context, Widget? child) {
-        return MaterialApp(
-          restorationScopeId: 'app',
-          localizationsDelegates: const [
-            AppLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: const [
-            Locale('en', ''),
-          ],
-          onGenerateTitle: (BuildContext context) =>
-              AppLocalizations.of(context)!.appTitle,
-          theme: ThemeData(),
-          darkTheme: ThemeData.dark(),
-          themeMode: settingsController.themeMode,
-          onGenerateRoute: (RouteSettings routeSettings) {
-            return MaterialPageRoute<void>(
-              settings: routeSettings,
-              builder: (BuildContext context) {
-                switch (routeSettings.name) {
-                  case SettingsView.routeName:
-                    return SettingsView(controller: settingsController);
-                  case CalorieLogView.routeName:
-                  default:
-                    return CalorieLogView(
-                      controller: calorieLogController,
-                    );
-                }
-              },
-            );
-          },
+    return MaterialApp(
+      restorationScopeId: 'app',
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''),
+      ],
+      onGenerateTitle: (BuildContext context) =>
+          AppLocalizations.of(context)!.appTitle,
+      theme: ThemeData(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
+      builder: (context, child) {
+        return CalorieLogView(
+          controller: calorieLogController,
         );
       },
     );
