e58ab7cce2dc0bcf939ac45fedbd20f6583fcb19
Ernesto Diaz <zaidotsenre@gmail.com>
2023-12-16 16:08:07 -0500

    Revert "Added launcher icons:"
    This reverts commit dde1f55fea360ee81b06657e2ecc3bdbfa3788ff.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -5,9 +5,17 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/services.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key, required this.calorieLogController});
+import 'settings/settings_controller.dart';
+import 'settings/settings_view.dart';
 
+/// The Widget that configures your application.
+class MyApp extends StatelessWidget {
+  const MyApp(
+      {super.key,
+      required this.settingsController,
+      required this.calorieLogController});
+
+  final SettingsController settingsController;
   final CalorieLogController calorieLogController;
 
   @override
@@ -16,25 +24,41 @@ class MyApp extends StatelessWidget {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-    return MaterialApp(
-      restorationScopeId: 'app',
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('en', ''),
-      ],
-      onGenerateTitle: (BuildContext context) =>
-          AppLocalizations.of(context)!.appTitle,
-      theme: ThemeData(),
-      darkTheme: ThemeData.dark(),
-      themeMode: ThemeMode.system,
-      builder: (context, child) {
-        return CalorieLogView(
-          controller: calorieLogController,
+    return ListenableBuilder(
+      listenable: settingsController,
+      builder: (BuildContext context, Widget? child) {
+        return MaterialApp(
+          restorationScopeId: 'app',
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('en', ''),
+          ],
+          onGenerateTitle: (BuildContext context) =>
+              AppLocalizations.of(context)!.appTitle,
+          theme: ThemeData(),
+          darkTheme: ThemeData.dark(),
+          themeMode: settingsController.themeMode,
+          onGenerateRoute: (RouteSettings routeSettings) {
+            return MaterialPageRoute<void>(
+              settings: routeSettings,
+              builder: (BuildContext context) {
+                switch (routeSettings.name) {
+                  case SettingsView.routeName:
+                    return SettingsView(controller: settingsController);
+                  case CalorieLogView.routeName:
+                  default:
+                    return CalorieLogView(
+                      controller: calorieLogController,
+                    );
+                }
+              },
+            );
+          },
         );
       },
     );
---
+++ lib/src/settings/settings_view.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import 'settings_controller.dart';
+
+/// Displays the various settings that can be customized by the user.
+///
+/// When a user changes a setting, the SettingsController is updated and
+/// Widgets that listen to the SettingsController are rebuilt.
+class SettingsView extends StatelessWidget {
+  const SettingsView({super.key, required this.controller});
+
+  static const routeName = '/settings';
+
+  final SettingsController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: DropdownButton<ThemeMode>(
+          value: controller.themeMode,
+          onChanged: controller.updateThemeMode,
+          items: const [
+            DropdownMenuItem(
+              value: ThemeMode.system,
+              child: Text('System Theme'),
+            ),
+            DropdownMenuItem(
+              value: ThemeMode.light,
+              child: Text('Light Theme'),
+            ),
+            DropdownMenuItem(
+              value: ThemeMode.dark,
+              child: Text('Dark Theme'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
